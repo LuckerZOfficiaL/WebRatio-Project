d522304e60c25e329840416c731937393e18d2fe
Luca Zhou <lucazhou2000@gmail.com>
2024-05-21 23:18:34 +0200

    add advanced grading formula for each LectureTaken

--- Main/DBScripts/DB_240521_230147.sql
+++ Main/DBScripts/DB_240521_230147.sql
@@ -1,10 +1,9 @@
 -- LectureTaken.grade [ent2#att20]
 drop view `lecturetaken_grade_view`;
 create view `lecturetaken_grade_view` as
-select AL1.`oid` as `oid`, sum(AL2.`iscorrect`) / sum(AL2.`attempts`) * count(distinct AL2.`oid`) / count(distinct AL3.`questionid`) as `der_attr`
+select AL1.`oid` as `oid`, avg(AL2.`iscorrect`) / avg(AL2.`attempts`) as `der_attr`
 from  `lecturetaken` AL1 
                left outer join `questiontaken` AL2 on AL1.`oid`=AL2.`lecturetaken_oid`
-               left outer join `question` AL3 on AL1.`lecture_lectureid`=AL3.`lecture_lectureid`
 group by AL1.`oid`;
 
 
